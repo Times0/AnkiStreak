bd9bf1aa119d8a9b8638d1cbf5ec2cf230bd05da
JeanJacques4269 <spotopaz@gmail.com>
2023-06-03 14:26:42 +0200

    Re added objects sinc I now have the magic update fonction. Fps drop when zooming in and out, might just disable the option if I dont find a fix

--- test_game/game.py
+++ test_game/game.py
@@ -7,49 +7,10 @@ import pytmx
 
 cwd = os.path.dirname(__file__)
 
-
-class Clickable:
-    def __init__(self, pos, size, name):
-        print(f"Creating clickable {name}")
-        self.size = size
-        self.name = name
-        self.rect = pygame.Rect(pos, size)
-
-        self.hovered = False
-        self.clicked = False
-
-        self.init_pos = pos
-        self.init_size = size
-
-    def handle_events(self, events):
-        for event in events:
-            if event.type == pygame.MOUSEMOTION:
-                pos = event.pos
-                if self.rect.collidepoint(pos):
-                    self.hovered = True
-                else:
-                    self.hovered = False
-
-    def update(self, camera_rect):
-        w, h = pygame.display.get_surface().get_size()
-        zoom = 1 / (camera_rect.w / w)
-        self.rect.width = self.init_size[0] * zoom
-        self.rect.height = self.init_size[1] * zoom
-
-        self.rect.x = self.init_pos[0] / (camera_rect.w / w) - camera_rect.x * zoom
-        self.rect.y = self.init_pos[1] / (camera_rect.h / h) - camera_rect.y * zoom
-
-    def draw(self, win):
-        pygame.draw.rect(win, RED, self.rect, 2)
-
-
-class Clickable_Object(Clickable):
-    def __init__(self, pos, size, name, img):
-        super().__init__(pos, size, name)
-        self.img = pygame.transform.scale(img, size)
-
-    def draw(self, win):
-        win.blit(self.img, self.rect)
+MAX_ZOOM = 2
+MIN_ZOOM = 1
+FPS = 60
+ALLOW_SCROOLING = False
 
 
 class GameObject:
@@ -59,8 +20,62 @@ class GameObject:
         self.img = pygame.transform.scale(img, size)
         self.rect = pygame.Rect(pos, size)
 
+        self.zoom_buffer = img
+
+    def update(self, camera_rect):
+        w, h = pygame.display.get_surface().get_size()
+        zoom = 1 / (camera_rect.w / w)
+        self.rect.width = self.size[0] * zoom
+        self.rect.height = self.size[1] * zoom
+
+        self.rect.x = self.pos[0] / (camera_rect.w / w) - camera_rect.x * zoom
+        self.rect.y = self.pos[1] / (camera_rect.h / h) - camera_rect.y * zoom
+
+        self.zoom_buffer = pygame.transform.scale(self.img, (int(self.size[0] * zoom), int(self.size[1] * zoom)))
+
     def draw(self, win):
-        win.blit(self.img, self.pos)
+        win.blit(self.zoom_buffer, self.rect)
+
+
+class Clickable:
+    def __init__(self, pos, size):
+        self.pos = pos
+        self.size = size
+        self.rect = pygame.Rect(pos, size)
+        self.hovered = False
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type == pygame.MOUSEMOTION:
+                if self.rect.collidepoint(event.pos):
+                    self.hovered = True
+                else:
+                    self.hovered = False
+
+    def update(self, camera_rect):
+        w, h = pygame.display.get_surface().get_size()
+        zoom = 1 / (camera_rect.w / w)
+        self.rect.width = self.size[0] * zoom
+        self.rect.height = self.size[1] * zoom
+
+        self.rect.x = self.pos[0] / (camera_rect.w / w) - camera_rect.x * zoom
+        self.rect.y = self.pos[1] / (camera_rect.h / h) - camera_rect.y * zoom
+
+    def draw(self, win):
+        if self.hovered:
+            pygame.draw.rect(win, RED, self.rect, 2)
+        else:
+            pygame.draw.rect(win, GREEN, self.rect, 2)
+
+
+class ClickableGameObject(GameObject, Clickable):
+    def __init__(self, pos, size, img):
+        super().__init__(pos, size, img)
+        Clickable.__init__(self, pos, size)
+
+    def draw(self, win):
+        GameObject.draw(self, win)
+        Clickable.draw(self, win)
 
 
 class Game:
@@ -69,46 +84,57 @@ class Game:
         self.running = True
 
         # ______________________TMX and pyscroll_____________________________________#
-        self.data_tmx = pytmx.load_pygame("../map.tmx")
+        self.data_tmx = pytmx.load_pygame("../map_with_objects.tmx")
         pyscroll_data = pyscroll.data.TiledMapData(self.data_tmx)
         self.map_layer = pyscroll.BufferedRenderer(pyscroll_data, self.win.get_size(), clamp_camera=True)
         self.clickable_objects = []
+        self.objects = []
         self.load_objects()
 
         # _____________________IDK___________________________________#
         self.scrolling = False
+        self.map_layer.zoom = 1.8
+        self.zoom_target = self.map_layer.zoom
 
     def load_objects(self):
         for obj in self.data_tmx.objects:
             if obj.type == "collision":
                 pos = (obj.x, obj.y)
                 size = (obj.width, obj.height)
-                self.clickable_objects.append(Clickable(pos, size, obj.name))
-            elif obj.type == "obj":
+                self.clickable_objects.append(Clickable(pos, size))
+            elif obj.type == "Building":
                 pos = (obj.x, obj.y)
                 size = (obj.width, obj.height)
                 img = obj.image
-                self.clickable_objects.append(Clickable_Object(pos, size, obj.name, img))
+                o = ClickableGameObject(pos, size, img)
+                self.clickable_objects.append(o)
+                self.objects.append(o)
+            elif obj.type in ["Tree", "Bridge", "Rock"]:
+                pos = (obj.x, obj.y)
+                size = (obj.width, obj.height)
+                img = obj.image
+                self.objects.append(GameObject(pos, size, img))
 
     def run(self):
         clock = pygame.time.Clock()
         while self.running:
-            clock.tick(60)
+            clock.tick(FPS)
+            print(f"\r{clock.get_fps()}", end="")
             self.events()
             self.update()
             self.draw()
 
     def handle_camera_events(self, events):
         for event in events:
-            if event.type == pygame.MOUSEWHEEL:
+            if event.type == pygame.MOUSEWHEEL and ALLOW_SCROOLING:
                 if event.y > 0:
-                    self.map_layer.zoom += 0.05
+                    self.zoom_target += 0.05
                 elif event.y < 0:
-                    self.map_layer.zoom -= 0.05
-                if self.map_layer.zoom < 1:
-                    self.map_layer.zoom = 1
-                if self.map_layer.zoom > 2:
-                    self.map_layer.zoom = 2
+                    self.zoom_target -= 0.05
+                if self.zoom_target < MIN_ZOOM:
+                    self.zoom_target = MIN_ZOOM
+                if self.zoom_target > MAX_ZOOM:
+                    self.zoom_target = MAX_ZOOM
                 self.map_layer.center(self.map_layer.view_rect.center)
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
@@ -135,14 +161,15 @@ class Game:
                 self.running = False
 
     def update(self):
-        for obj in self.clickable_objects:
+        if self.map_layer.zoom != self.zoom_target:
+            self.map_layer.zoom += (self.zoom_target - self.map_layer.zoom) / 5
+            self.map_layer.center(self.map_layer.view_rect.center)
+        for obj in self.objects:
             obj.update(self.map_layer.view_rect)
 
     def draw(self):
         self.win.fill(BLACK)
-        # self.map_layer.center(self.map_layer.view_rect.center)
-        print(self.map_layer.view_rect.center)
         self.map_layer.draw(self.win, self.win.get_rect())
-        for obj in self.clickable_objects:
+        for obj in self.objects:
             obj.draw(self.win)
         pygame.display.update()
