c2a1878cf1e1e6c01870a5f32e54030030ac89f2
Times0 <45049767+Times0@users.noreply.github.com>
2023-12-17 16:58:57 +0100

    readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# AnkiStreak
+
+![img.png](img.png)
+
+# How to run
+
+```bash
+git clone this repo
+cd AnkiStreak
+python build.py
+```
+
+The build tool will put the game in the anki addon folder.
+It will also set the debug mode to False.
+
+It will install all the requirements that anki needs to run the game in Python 3.9.
+
+If you get errors (for example when installing scipy). 
+
--- build.py
+++ build.py
@@ -1,8 +1,10 @@
 import os
 import shutil
+import subprocess
 
-anki_ext_dir = os.path.join(os.getenv('APPDATA'), 'Anki2', 'addons21', 'ankistreak')
-print(anki_ext_dir)
+LIGHT_MODE = False  # No installation of Librairies in light mode
+
+anki_ext_dir = os.path.join(os.getenv('APPDATA'), 'Anki2', 'addons21', 'ankistreak_test')
 
 if os.path.exists(os.path.join(anki_ext_dir, "test_game")):
     shutil.rmtree(os.path.join(anki_ext_dir, "test_game"))
@@ -12,7 +14,8 @@ if os.path.exists(os.path.join(anki_ext_dir, "__init__.py")):
     os.remove(os.path.join(anki_ext_dir, "__init__.py"))
 shutil.copy("__init__.py", anki_ext_dir)
 
-# change debug to false
+shutil.copy("requirements.txt", anki_ext_dir)
+
 with open(os.path.join(anki_ext_dir, "test_game", "boring", "config.py"), "r") as f:
     lines = f.readlines()
     for i, line in enumerate(lines):
@@ -22,6 +25,11 @@ with open(os.path.join(anki_ext_dir, "test_game", "boring", "config.py"), "r") a
 with open(os.path.join(anki_ext_dir, "test_game", "boring", "config.py"), "w") as f:
     f.writelines(lines)
 
-# removes the saves in the game_data directory without removing the directory
 for file in os.listdir(os.path.join(anki_ext_dir, "test_game", "game_data")):
     os.remove(os.path.join(anki_ext_dir, "test_game", "game_data", file))
+
+if not LIGHT_MODE:
+    subprocess.run(["pip3.9", "install", "-r", "requirements.txt", "--target", '.'], cwd=anki_ext_dir, shell=True)
+    os.remove(os.path.join(anki_ext_dir, "demo.py"))
+
+print("Done!")
